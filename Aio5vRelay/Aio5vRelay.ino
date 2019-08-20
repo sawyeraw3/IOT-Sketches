9598baa798a9a9f58292f5e57def613af08a9b30
Sawyer Warden <sawyer.warden@gmail.com>
2019-08-19 22:45:23 -0400

    WebOTA enabled

--- Aio5vRelay/Aio5vRelay.ino
+++ Aio5vRelay/Aio5vRelay.ino
@@ -1,15 +1,18 @@
-// Simple sketch for controlling a 5v relay attached to esp8266-12E via Adafruit IO
-// The relay is powered via the Vin on the esp, and controls a 110v outlet attached to its NC terminal.
-
 #include "config.h"
 #ifdef __AVR__
   #include <avr/power.h>
 #endif
 
+#include <WebOTA.h>
+#include <ESP8266WiFi.h>
+
+#define UPDATE_PORT 8080
+#define UPDATE_LINK "/update"
 #define RELAY_PIN 5
 #define ONBOARD_LED 16
+#define ESP_ID "esp0"
 
-AdafruitIO_Feed *relay_feed = io.feed("relay");
+AdafruitIO_Feed *home_iot = io.feed("home_iot");
 
 void setup() {
   pinMode(RELAY_PIN, OUTPUT);
@@ -17,15 +20,22 @@ void setup() {
 
   Serial.begin (115200);
   io.connect();
-  relay_feed->onMessage(handleRelayMessage);
+  home_iot->onMessage(handleMessage);
+  
   // Wait for a connection
   while(io.status() < AIO_CONNECTED) {
     Serial.print(".");
-    delay(1);
+    delay(10);
   }
+
+  webota.init(UPDATE_PORT, UPDATE_LINK);
+  
   // Successfully connected
   Serial.println();
   Serial.println(io.statusText());
+  
+  String intitializedStatusString = WiFi.localIP().toString() + ":" + UPDATE_PORT + UPDATE_LINK + " to update";
+  Serial.println(intitializedStatusString);
 
   // Set analogWrite range for ESP8266
   #ifdef ESP8266
@@ -35,26 +45,31 @@ void setup() {
   // Outlet is connected to NC and powered when relay is off
   // so LED indicates outlet 'on'
   digitalWrite (ONBOARD_LED, 0);
+
+  home_iot->save((String)ESP_ID + " | " + intitializedStatusString);
 }
 
 void loop() {
-  // Always listen for MQTT messages from AdafruitIO feeds
   io.run();
+  webota.handle();
 }
 
-void handleRelayMessage(AdafruitIO_Data *data) {
-  String s = data->toString();
-  // Could splice s before analysis
-  if(s == "relay:1") {
-    // Relay turned on, so outlet is 'off'
-    toggle_relay_and_led(1);
-  } else if(s == "relay:0") {
-    toggle_relay_and_led(0);
+void handleMessage(AdafruitIO_Data *data) {
+  String str = data->toString();
+  String targetEsp = str.substring(0, str.indexOf(":"));
+  int val = str.substring(str.indexOf(":") + 1).toInt();
+  if (targetEsp == ESP_ID) {
+    handleRelayMessage(val);
   }
 }
 
-void toggle_relay_and_led(int i) {
-  digitalWrite (RELAY_PIN, i);
-  digitalWrite (ONBOARD_LED, i);
+void handleRelayMessage(int i) {
+  if(i) {
+    // Relay turned on, so outlet is 'off'
+    digitalWrite (RELAY_PIN, 1);
+    digitalWrite (ONBOARD_LED, 1);
+  } else {
+    digitalWrite (RELAY_PIN, 0);
+    digitalWrite (ONBOARD_LED, 0);
+  }
 }
-
